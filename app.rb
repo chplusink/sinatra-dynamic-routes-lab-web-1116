cf325020070a45d7df90b01feb0027a2d8b4921b
Vic Lam <lam.vic.k@gmail.com>
2016-12-19 13:42:16 -0500

    mutiply != multiply

--- app.rb
+++ app.rb
@@ -1,4 +1,36 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/reversename/:name' do
+    params[:name].reverse
+  end
+
+  get '/square/:number' do
+    "#{params[:number].to_i * params[:number].to_i}"
+  end
+
+  get '/say/:number/:phrase' do
+    @say = ""
+    params[:number].to_i.times {@say = @say + params[:phrase] + "\n"}
+    @say
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    params[:captures].join(' ') + '.'
+  end
+
+  get '/:operation/:number1/:number2' do
+    case params[:operation]
+    when 'add'
+      (params[:number1].to_i + params[:number2].to_i).to_s
+    when 'subtract'
+      (params[:number1].to_i - params[:number2].to_i).to_s
+    when 'multiply'
+      (params[:number1].to_i * params[:number2].to_i).to_s
+    when 'divide'
+      (params[:number1].to_i / params[:number2].to_i).to_s
+    end
+  end
+
 end
